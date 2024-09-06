4c3b44e0cac5a7fab452eae0cc094c8b72e67769
Asma-malica <115386995+Asma-malica@users.noreply.github.com>
2024-09-06 17:54:45 +0530

    update

---
+++ EventRegistration/carousel.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+
+function moveSlide(direction) {
+    const carousel = document.querySelector('.carousel');
+    const slides = document.querySelectorAll('.gallery-img');
+    const slideWidth = slides[0].clientWidth + 20; 
+    slideIndex += direction;
+
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 3;
+    } else if (slideIndex > slides.length - 3) {
+        slideIndex = 0;
+    }
+
+    carousel.style.transform = `translateX(${-slideIndex * slideWidth}px)`;
+}
